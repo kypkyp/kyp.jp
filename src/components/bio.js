d151889a46710eeca3f2c3a14643cf7fb857cb3a
kyp <kyp@kmc.gr.jp>
2021-06-12 01:31:08 +0900

    0611

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,18 +6,11 @@
  */
 
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const Bio = () => {
-  const icon = {
-    twitter: <FontAwesomeIcon icon={faTwitter} />,
-    github: <FontAwesomeIcon icon={faGithub} />,
-  }
-
   return (
     <div className="bio">
-      <a href="https://twitter.com/_kypu_">Twitter </a>/
+      <a href="https://twitter.com/_kypu_">Twitter</a>
       <a href="https://github.com/kypkyp">Github</a>
     </div>
   )
