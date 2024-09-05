476a59efd5c9cf0a35aba3decf30c315aaf1c414
kypkyp <kyp@kmc.gr.jp>
2024-09-06 01:13:09 +0900

    星座

--- src/components/bio.js
+++ src/components/bio.js
@@ -10,12 +10,8 @@ import React from "react"
 const Bio = () => {
   return (
     <div className="bio">
-      <p>Webエンジニア兼インディーゲーム作者です。変なものが好き。</p>
-      <p>
-        <a href="https://safehavn.dev/">SAFE HAVN STUDIO</a>
-        <a href="https://twitter.com/_newkyp">Twitter</a>
-        <a href="https://github.com/kypkyp">Github</a>
-      </p>
+      <a href="https://twitter.com/_newkyp">Twitter</a>
+      <a href="https://safehavn.dev/">Studio</a>
     </div>
   )
 }
